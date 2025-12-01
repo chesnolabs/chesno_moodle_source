2a2791ae8bd9dbb74f9b855f7f88339d4e7ac143
Ivan <jkurero@gmail.com>
2025-12-01 15:10:47 +0200

    Update Moodle configuration file to enhance security and environment variable usage

--- config.php
+++ config.php
@@ -1,40 +1,39 @@
-<?php
+<?php  // Moodle configuration file
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!!															   !!!
+// !!!  CAUTION!!! DON'T PUT SENSITIVE INFORMATION INTO THIS FILE  !!!
+// !!!			IT GOES TO THE GITHUB REPOSITORY			   	   !!!
+// !!! PUT PASSWORDS TO THE .env FILE AND GET THEM HERE VIA getenv !!!
+// !!!															   !!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = getenv('MOODLE_DBTYPE');
+$CFG->dbtype    = getenv('MOODLE_DB_TYPE');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('MOODLE_DBHOST');
-$CFG->dbname    = getenv('MOODLE_DBNAME');
-$CFG->dbuser    = getenv('MOODLE_DBUSER');
-$CFG->dbpass    = getenv('MOODLE_DBPASS');
-$CFG->prefix    = getenv('MOODLE_PREFIX');
-$CFG->dboptions = array(
-    'dbpersist' => false,
-    'dbsocket'  => false,
-    'dbport'    => getenv('MOODLE_DBPORT'),
-    'dbhandlesoptions' => false,
-    'dbcollation' => 'utf8mb4_unicode_ci',
-);
+$CFG->dbhost    = getenv('MOODLE_DB_HOST');
+$CFG->dbname    = getenv('MOODLE_DB_NAME');
+$CFG->dbuser    = getenv('MOODLE_DB_USER');
+$CFG->dbpass    = getenv('MOODLE_DB_PASS');
+$CFG->prefix    = getenv('MOODLE_DB_PREFIX');
+$CFG->dboptions = [
+  'dbpersist' => 0,
+  'dbport' => getenv('MOODLE_DB_PORT'),
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+];
 
 $CFG->wwwroot   = getenv('MOODLE_WWWROOT');
 $CFG->dataroot  = getenv('MOODLE_DATAROOT');
 $CFG->admin     = 'admin';
 
-$CFG->directorypermissions = 02777;
+$CFG->directorypermissions = 0777;
 
-// Upgrade key
-$CFG->upgradekey = getenv('MOODLE_UPGRADEKEY');
-
-// Password pepper
-$passwordpepper = getenv('MOODLE_PASSWORD_PEPPER');
-if ($passwordpepper) {
-    $CFG->passwordpeppers = [
-        1 => $passwordpepper
-    ];
-}
-
+// Set upgrade key and password pepper from .env if they exist
+$CFG->upgradekey = getenv('MOODLE_UPGRADEKEY') ?: null;
+$CFG->passwordpepper = getenv('MOODLE_PASSWORD_PEPPER') ?: null;
 
 require_once(__DIR__ . '/lib/setup.php');
 
